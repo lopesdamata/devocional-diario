aae2e6ae8f62a058fffba0518f2a55b38f7838eb
lopesdamata <lopesdamata@gmail.com>
2025-11-17 14:05:44 -0300

    Create LoadingSpinner.tsx

---
+++ omponents/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function LoadingSpinner() {
+  return (
+    <div className="w-8 h-8 border-4 border-slate-200 border-t-slate-600 rounded-full animate-spin"></div>
+  );
+}
